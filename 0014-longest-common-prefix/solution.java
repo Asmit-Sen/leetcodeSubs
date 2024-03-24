fe09732e99cae2f2eae128368ccc6da280c06515
Asmit Sen <susmita.sen1130@gmail.com>
2024-03-24 18:11:11 +0000

    [LeetCode Sync] Runtime - 1 ms (61.01%), Memory - 41.4 MB (90.19%)

---
+++ 0014-longest-common-prefix/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        StringBuilder ans=new StringBuilder();
+        String first=strs[0];
+        String last=strs[strs.length-1];
+        for(int i=0;i<(int)Math.min(first.length(),last.length());i++)
+        {
+            if(first.charAt(i)==last.charAt(i))
+            ans.append(first.charAt(i));
+            else break;
+        }
+        return ans.toString();
+
+    }
+}
