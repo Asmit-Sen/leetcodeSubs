f7578b96ad8e0763ea90cc9db58621b2bddc4d96
Asmit Sen <susmita.sen1130@gmail.com>
2024-06-29 18:21:15 +0000

    [LeetCode Sync] Runtime - 2 ms (75.20%), Memory - 52.9 MB (69.71%)

--- 0169-majority-element/solution.java
+++ 0169-majority-element/solution.java
@@ -1,22 +1,17 @@
 class Solution {
-    public int majorityElement(int[] nums) {
-        int m=nums[0];
-        int c=0,max=0;
-        int max_el=-1;
-        for (int it:nums)
-        {
-            
-            if (c==0) m=it;
-            if(it==m) c++;
+    public int majorityElement(int[] arr) {
+
+         int c=0,el=-1;
+         for(int i=0;i<arr.length;i++)
+         {
+            if(c==0) el=arr[i];
+            if(arr[i]==el) c++;
             else c--;
-        }
-        if(c>0) max_el=m;
-        int count=0;
-        for (int it:nums)
-        if(it==max_el) count++;
-        if(count>nums.length/2) return max_el;
-        else return -1;
-
-
+         }       
+         int cnt=0;
+         for(int num:arr)
+         if(num==el) cnt++;
+         if(cnt>(arr.length/2)) return el;
+         else return -1;
     }
 }
