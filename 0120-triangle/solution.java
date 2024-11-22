c7c1a4474956a3f9176c9cd70dcb3f55ac758eb6
Asmit Sen <susmita.sen1130@gmail.com>
2024-11-22 15:48:37 +0000

    [LeetCode Sync] Runtime - 5 ms (46.11%), Memory - 44.3 MB (99.96%)

--- 0120-triangle/solution.java
+++ 0120-triangle/solution.java
@@ -9,10 +9,31 @@ class Solution {
         return dp[i][j]=(int)Math.min(d,dr);//memo
     }
     public int minimumTotal(List<List<Integer>> list) {
-        int dp[][]=new int[list.size()-1][list.size()-1];
-        for(int row[]:dp)
-        Arrays.fill(row,-1);//memo
+        // int dp[][]=new int[list.size()-1][list.size()-1];
+        // for(int row[]:dp)
+        // Arrays.fill(row,-1);//memo
         
-        return path(0,0,list,dp);
+        // return path(0,0,list,dp);
+
+        int n=list.size();
+        int dp[][]=new int[n][n];
+
+        if(n==1) return list.get(0).get(0);
+        
+        //base case is the last row
+        for(int j=0;j<n;j++)
+        {
+            dp[n-1][j]=list.get(n-1).get(j);
+        }
+        for(int i=n-2;i>=0;i--) 
+        {
+            for(int j=i;j>=0;j--)
+            {
+                int d=list.get(i).get(j)+ dp[i+1][j];
+                int dr=list.get(i).get(j)+dp[i+1][j+1];
+                dp[i][j]=(int)Math.min(d,dr);
+            }
+        }
+        return dp[0][0];
     }
 }
