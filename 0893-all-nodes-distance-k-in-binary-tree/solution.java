0b4429ca5e9fc50ae5138dee2240821bd4bbb9c5
Asmit Sen <susmita.sen1130@gmail.com>
2025-02-17 12:17:18 +0000

    [LeetCode Sync] Runtime - 16 ms (7.08%), Memory - 42.4 MB (93.02%)

--- 0893-all-nodes-distance-k-in-binary-tree/solution.java
+++ 0893-all-nodes-distance-k-in-binary-tree/solution.java
@@ -8,59 +8,40 @@
  * }
  */
 class Solution {
-    void bfs1(TreeNode root, Map<TreeNode,TreeNode> map)
+    void dfs(TreeNode root, Map<TreeNode, TreeNode> map)
     {
-        Queue<TreeNode> q=new LinkedList<>();
-        q.offer(root);
+        if(root==null) return;
 
-        while(!q.isEmpty())
-        {
-            root=q.poll();
-            if(root.left!=null)
-            {
-                map.put(root.left,root);
-                q.offer(root.left);
-            }
-            if(root.right!=null)
-            {
-                map.put(root.right,root);
-                q.offer(root.right);
-            }
-        }
+        if(root.left!=null)
+        map.put(root.left,root);
+        if(root.right!=null)
+        map.put(root.right,root);
+
+        dfs(root.left,map);
+        dfs(root.right,map);
     }
-    void bfs2(TreeNode root, Map<TreeNode,TreeNode> map, int k, List<Integer> ans)
+
+    void f(TreeNode root, int k, List<Integer> ans, List<TreeNode> visited, Map<TreeNode, TreeNode> map)
     {
-        Queue<TreeNode> q=new LinkedList<>();
-        q.offer(root);
-        Set<TreeNode> set=new HashSet<>();
-        while(k>0 && !q.isEmpty())
-        {
-            int n=q.size();
-            for(int i=0;i<n;i++)
-            {
-                root=q.poll();
-                set.add(root); // mark curr as visited
-                if(root.left!=null && !set.contains(root.left))
-                    q.offer(root.left);
-                
-                if(root.right!=null && !set.contains(root.right))
-                    q.offer(root.right);
-
-                if(map.containsKey(root) && !set.contains(map.get(root)))
-                    q.offer(map.get(root));
-            }// decrese dist after after every radial traversal
-            k--;
-        }
-        //when k=0, the level we have in queue is at dist k
-        while(!q.isEmpty())
-            ans.add(q.poll().val);
+        if(root==null) return;
+        
+        visited.add(root);
+        if(k==0) 
+            ans.add(root.val);
+        
+        if(map.containsKey(root) && !visited.contains(map.get(root))) f(map.get(root),k-1,ans,visited,map);
+        if(root.left!=null && !visited.contains(root.left)) f(root.left,k-1,ans,visited,map);
+        if(root.right!=null && !visited.contains(root.right)) f(root.right,k-1,ans,visited,map);
     }
+
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        //radial traverse
+        Map<TreeNode,TreeNode> map=new HashMap<>();
+        //dfs
+        dfs(root,map);
         List<Integer> ans=new ArrayList<>();
-        if(root==null) return ans;
-        Map<TreeNode,TreeNode> map= new HashMap<>();
-        bfs1(root,map); // children-> parent
-        bfs2(target,map,k,ans);
+
+        f(target,k,ans,new ArrayList<>(),map);
         return ans;
     }
 }
