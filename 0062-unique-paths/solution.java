a8e75b9ab2996086e1cf794b6da310a5f2ba2ec6
Asmit Sen <susmita.sen1130@gmail.com>
2025-02-27 18:51:11 +0000

    [LeetCode Sync] Runtime - 1 ms (11.36%), Memory - 40.5 MB (85.74%)

--- 0062-unique-paths/solution.java
+++ 0062-unique-paths/solution.java
@@ -1,35 +1,33 @@
 class Solution {
-    int path (int m, int n,int dp[][])
+    int f(int m, int n)
     {
-        if(m==0 && n==0) return 1;
         if(m<0 || n<0) return 0;
+        if(m==0 && n==0) return 1;
 
-        if(dp[m][n]!=-1) return dp[m][n]; //memo
-        
-        int l=path(m-1,n,dp); //up
-        int r=path(m,n-1,dp); //left
-
-        return dp[m][n]=l+r;//memo
+        return f(m-1,n)+f(m,n-1);
     }
     public int uniquePaths(int m, int n) {
-        int dp[][]=new int[m][n];// memo
-        for(int[] row:dp)
-        Arrays.fill(row,-1);
+        int dp[][]=new int[m][n];
+        for(int row[]:dp)
+        Arrays.fill(row,0);
 
-        for(int i=0;i<m;i++)// tabulation
+        int l=0,r=0;
+        for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
                 if(i==0 && j==0) dp[i][j]=1;
-                else 
+                else
                 {
-                    int l=0,r=0;
+                    l=0;r=0;
                     if(i>0) l=dp[i-1][j];
                     if(j>0) r=dp[i][j-1];
-                    dp[i][j]=l + r;
+                    dp[i][j]=l+r;
                 }
             }
         }
         return dp[m-1][n-1];
+
+        // return f(m-1,n-1);
     }
 }
