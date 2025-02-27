eab765d4acf2b35d403f6aa4639d1e11ff64039c
Asmit Sen <susmita.sen1130@gmail.com>
2025-02-27 18:25:19 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.8 MB (95.75%)

--- 0198-house-robber/solution.java
+++ 0198-house-robber/solution.java
@@ -1,22 +1,31 @@
 class Solution {
-    int f(int arr[], int idx, int dp[])
+    int f(int arr[], int i, int dp[])
     {
-        if(idx==0) return arr[0];
-        if(dp[idx]!=-1) return dp[idx]; //memo
-        
-        // take
-        int l=arr[idx];
-        if(idx>1) l+=f(arr,idx-2,dp);
+        if(i<0) return 0;
+        if(i==0) return arr[0];
 
-        //not take
-        int r=f(arr,idx-1,dp);
+        if(dp[i]!=-1) return dp[i];
 
-        return dp[idx]=(int)Math.max(l,r); //memo
+        return dp[i]=(int)Math.max(arr[i]+f(arr,i-2,dp),f(arr,i-1,dp));
     }
     public int rob(int[] nums) {
-        int dp[]=new int[nums.length];
-        Arrays.fill(dp,-1);
+        // int dp[]=new int[nums.length+1];
+        // Arrays.fill(dp,-1);
 
-        return f(nums,nums.length-1,dp);
+        // return f(nums,nums.length-1,dp);
+
+        // dp[0]=nums[0];
+        // int pick=0,notpick=0;
+        int prev2=0,prev=nums[0],curr=0;
+
+        for(int i=1;i<nums.length;i++)
+        {
+            curr=(int)Math.max(nums[i]+prev2,prev);
+
+            prev2=prev;
+            prev=curr;
+        }
+        return prev;
+
     }
 }
