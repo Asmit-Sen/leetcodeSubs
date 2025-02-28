a9509aedbcd383f8e8142252e5fad04ccd6808d0
Asmit Sen <susmita.sen1130@gmail.com>
2025-02-28 15:39:12 +0000

    [LeetCode Sync] Runtime - 4 ms (61.84%), Memory - 48.4 MB (58.87%)

--- 0064-minimum-path-sum/solution.java
+++ 0064-minimum-path-sum/solution.java
@@ -1,36 +1,24 @@
 class Solution {
-    int path(int m, int n, int grid[][], int dp[][])
-    {
-        if(m==0 && n==0) return grid[0][0];
-        if(m<0 || n<0) return (int)Math.pow(10,9);
-        if(dp[m][n]!=-1) return dp[m][n];//memo
-
-        int up= grid[m][n]+ path(m-1,n,grid,dp);
-        int left=grid[m][n]+ path(m,n-1,grid,dp);
-        return dp[m][n]=(int)Math.min(up,left);//memo
-
-    }
     public int minPathSum(int[][] grid) {
         int m=grid.length,n=grid[0].length;
-        
-        int dp[][]=new int[m][n];//memo
-        for(int row[]:dp)
-        Arrays.fill(row,-1);
-        
+        // int dp[][]=new int[m][n];
+        int prev[]=new int[n], cur[]=new int[n];
+
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                if(i==0 && j==0) dp[i][j]=grid[0][0];
-                else 
+                if(i==0 && j==0) cur[j]=grid[i][j];
+                else
                 {
-                    int up=(int)Math.pow(10,9),left=(int)Math.pow(10,9);//we start with default large values so it doesnt overflow or disturb our min path sum
-                    if(i>0)up=grid[i][j]+dp[i-1][j];
-                    if(j>0)left=grid[i][j]+dp[i][j-1];
-                    dp[i][j]=(int)Math.min(up,left);
+                    int up=Integer.MAX_VALUE,left=Integer.MAX_VALUE;
+                    if(i>0) up=prev[j];
+                    if(j>0) left=cur[j-1];
+                    cur[j]=(int)Math.min(up,left)+grid[i][j];
                 }
+                prev=cur;
             }
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 }
