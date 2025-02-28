075cac28911b98349f4fa5fb152073377e6f7d86
Asmit Sen <susmita.sen1130@gmail.com>
2025-02-28 15:31:52 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.5 MB (98.89%)

--- 0063-unique-paths-ii/solution.java
+++ 0063-unique-paths-ii/solution.java
@@ -1,40 +1,30 @@
 class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m=obstacleGrid.length,n=obstacleGrid[0].length;
+        int dp[][]=new int[m][n];
 
-    int path (int m, int n,int arr[][], int dp[][])
-    {
-        if(m<0 || n<0 || arr[m][n]==1) return 0;//-ve index or a block
-        if(m==0 && n==0) return 1;
+        int prev[]=new int[n], curr[]=new int[n];
 
-        if(dp[m][n]!=-1) return dp[m][n];//memo
-    
-        int l=path(m-1,n,arr,dp); //up
-        int r=path(m,n-1,arr,dp); //left
-
-        return dp[m][n]=l+r;//memo
-    }
-    
-    public int uniquePathsWithObstacles(int[][] arr) {
-        int m=arr.length,n=arr[0].length;
-        int dp[][]=new int[m][n];//memo
-        for(int row[]:dp)
-        Arrays.fill( row,-1);
-
+        // dp[0][0]=1;
+        // dp[i][j]=0 if grid[i][j]=1
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                if(arr[i][j]==1) dp[i][j]=0;
-                else if( i== 0 && j==0) dp[i][j]=1;
-                else
-                {
-                    int up=0,left=0;
-                    if(i>0) up=dp[i-1][j];
-                    if(j>0) left=dp[i][j-1];
-                    dp[i][j]=up+left;
+                if(obstacleGrid[i][j]==1) curr[j]=0;
+                else{
+                    if(i==0 && j==0) curr[j]=1;
+                    else
+                    {
+                        int left=0;
+                        // if(i>0) up=prev[j];
+                        if(j>0) left=curr[j-1];
+                        curr[j]=prev[j]+left;
+                    }
+                    prev=curr;
                 }
             }
         }
-        return dp[m-1][n-1];
-        
+        return prev[n-1];
     }
 }
