b53f52ab2af01126fb9ed2ed82e30c2509b33737
Asmit Sen <susmita.sen1130@gmail.com>
2025-03-01 19:23:43 +0000

    [LeetCode Sync] Runtime - 19 ms (37.23%), Memory - 45.5 MB (64.26%)

--- 0322-coin-change/solution.java
+++ 0322-coin-change/solution.java
@@ -1,55 +1,39 @@
 class Solution {
-    int f(int coins[],int i, int amount, int dp[][])
+    int f(int i, int arr[], int amt, int dp[][])
     {
+        if(amt==0) return 0;
+        if(i==0) return (amt%arr[0]==0)?amt/arr[0]:(int)1e9;
+        if(dp[i][amt]!=-1) return dp[i][amt];
 
-        // if(amount==0) return 0;// no more coins needed 
-        // if(i<0) return (int)1e9; 
-        if(amount==0) return 0;
-        if(i==0)
-        {
-            if(amount%coins[i]==0)
-            return amount/coins[i];
-            return (int)1e9;
-        }
+        int l=(int)1e9,r=(int)1e9;
+        l=f(i-1,arr,amt,dp);
+        if(amt>=arr[i]) r=1+f(i,arr,amt-arr[i],dp);
 
-        if(dp[i][amount]!=-1)
-        return dp[i][amount];
-
-        int take=(int)1e9;// INT_MAX can overflow
-        if(coins[i]<=amount) take= 1 + f(coins,i,amount-coins[i],dp);
-        int not_take=f(coins,i-1,amount,dp);
-
-        return dp[i][amount]=(int)Math.min(take,not_take);
+        return dp[i][amt]=(int)Math.min(l,r);
     }
     public int coinChange(int[] coins, int amount) {
         int dp[][]=new int[coins.length][amount+1];
         for(int row[]:dp)
-        Arrays.fill(row,-1);
-
-        // int ans=f(coins,coins.length-1,amount,dp);
-        // if(ans>=(int)1e9) return -1;
-        // return ans;
+        Arrays.fill(row,(int)1e9);
+        
+        for(int i=0;i<coins.length;i++) 
+        dp[i][0]=0;
         
         for(int j=0;j<=amount;j++)
-        dp[0][j]=(j%coins[0]==0)?j/coins[0]:(int)1e9; //if only a single coin available, check if divisible
+        dp[0][j]=(j%coins[0]==0)?j/coins[0]:(int)1e9;
 
-        for(int i=0;i<coins.length;i++)
-        dp[i][0]=0;// if amt=0, no coins needed
+        int l=(int)1e9,r=(int)1e9;
 
-        
         for(int i=1;i<coins.length;i++)
         {
             for(int amt=1;amt<=amount;amt++)
             {
-                int take=(int)1e9;// INT_MAX can overflow
-                if(coins[i]<=amt) take= 1 + dp[i][amt-coins[i]];
-                int not_take=dp[i-1][amt];
-
-                dp[i][amt]=(int)Math.min(take,not_take);
+                l=dp[i-1][amt];
+                if(amt>=coins[i]) r=1+dp[i][amt-coins[i]];
+                dp[i][amt]=(int)Math.min(l,r);
             }
         }
-
-        int ans= dp[coins.length-1][amount];
-        return ans==(int)1e9?-1:ans;
+        int ans=dp[coins.length-1][amount];
+        return (ans==(int)1e9)?-1:ans;
     }
 }
