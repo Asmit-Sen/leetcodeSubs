023f7fc3e02fdb7f795ea6389fbafee12d7f133a
Asmit Sen <susmita.sen1130@gmail.com>
2025-03-03 18:53:42 +0000

    [LeetCode Sync] Runtime - 12 ms (5.10%), Memory - 45.1 MB (97.29%)

--- 0735-asteroid-collision/solution.java
+++ 0735-asteroid-collision/solution.java
@@ -1,55 +1,27 @@
 class Solution {
-    public int[] asteroidCollision(int[] arr) {
-        //Stack <Integer> st=new Stack<>();
-        List <Integer> list=new ArrayList<>();
-        // for(int num:asteroids)
-        // {
-        //     if(st.isEmpty() || num>0)
-        //         {
-        //             st.add(num);
-        //             continue;
-        //         }
-
-        //     if(num<0)
-        //     {
-        //         if(st.get(st.size()-1)<0) 
-        //             {
-        //                 st.add(num); //put -ve in if the topis -ve, hence no collision;
-        //                 continue;
-        //             }
-
-        //         while(!st.isEmpty() && st.get(st.size()-1)<(int)Math.abs(num))
-        //         st.remove(st.size()-1); //pop all the smaller +ve asteroids
-
-        //         if(st.isEmpty()) st.add(num); // put -ve only if all smaller +ve are out
-        //         // else the larger +ve remain and the negative is skipped
-        //         else 
-        //             if((st.get(st.size()-1)==-num))
-        //             st.remove(st.size()-1); // equals get destroyed
-
-        //     }
-        // }
-
-        for(int i=0;i<arr.length;i++)
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack <Integer> st=new Stack<>();
+        for(int asteroid:asteroids)
         {
-            if(arr[i]>0)
-            list.add(arr[i]);
-            else // arr[i] is -ve
+            if(asteroid>0)
+            st.push(asteroid);
+            else
             {
-                while(list.size()!=0 && list.get(list.size()-1)>0 && list.get(list.size()-1)<-(arr[i]))
-                list.remove(list.size()-1);
-                if(list.size()==0 || list.get(list.size()-1)<0)
-                    list.add(arr[i]);
-                else if (list.get(list.size()-1) == -arr[i])
-                    list.remove(list.size()-1);
-                else 
-                    continue;
+                while(!st.isEmpty() && st.peek()>0 && st.peek()<(int)Math.abs(asteroid))
+                st.pop();
+
+                if(st.isEmpty() || st.peek()<0) st.push(asteroid);
+                else if(st.peek()==(int)Math.abs(asteroid)) 
+                {
+                    st.pop();
+                }
+                else continue;
             }
         }
-
-        int ans[]=new int[list.size()];
-        for(int i=0;i<list.size();i++)
-        ans[i]=list.get(i);
-        return ans;
+        System.out.println(st);
+        int arr[]=new int[st.size()];
+        for(int i=st.size()-1;i>=0;i--)
+        arr[i]=st.pop();
+        return arr;
     }
 }
