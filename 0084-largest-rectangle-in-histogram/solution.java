c306e51dcf227bda5b79cf312cb8ebe38d91d79f
Asmit Sen <susmita.sen1130@gmail.com>
2025-03-04 16:49:08 +0000

    [LeetCode Sync] Runtime - 60 ms (77.78%), Memory - 57.3 MB (88.37%)

--- 0084-largest-rectangle-in-histogram/solution.java
+++ 0084-largest-rectangle-in-histogram/solution.java
@@ -1,38 +1,28 @@
 class Solution {
     public int largestRectangleArea(int[] arr) {
-        int n=arr.length, maxi=0;
-        Stack <Integer> stack=new Stack<>();
-        // int pse[]=new int[n];
-        // int nse[]=new int[n];
-        int nse,pse;
-        for(int i=0;i<n;i++)
+        Stack <Integer> st=new Stack<>();
+        //pse and nse index
+        int pse, nse, maxi=0;
+        for(int i=0;i<arr.length;i++)
         {
-            while(!stack.isEmpty() && arr[stack.peek()]>=arr[i])
+            while(!st.isEmpty() && arr[st.peek()]>=arr[i]) 
             {
-                //before popping do calc
-                int top=stack.peek();
-                stack.pop();
-
+                int top=arr[st.pop()];
                 nse=i;
-                pse=stack.isEmpty()?-1:stack.peek();;
-                maxi=(int)Math.max(maxi,(nse-pse-1)*arr[top]);
-                
-            }
-            stack.push(i);
-        }
+                pse=st.isEmpty()?-1:st.peek();
 
-        while(!stack.isEmpty())
+                maxi=(int)Math.max(maxi,(nse-pse-1)*top);
+            }
+            st.push(i);
+        }
+        while(!st.isEmpty())
         {
-            int top=stack.peek();
-            stack.pop();
-            
-            nse=n;
-            pse=stack.isEmpty()?-1:stack.peek();
-            maxi=(int)Math.max(maxi,(n-pse-1)*arr[top]);
-    
-            
+            int top=arr[st.pop()];
+            nse=arr.length;
+            pse=st.isEmpty()?-1:st.peek();
+
+            maxi=(int)Math.max(maxi,(nse-pse-1)*top);
         }
         return maxi;
-
     }
 }
