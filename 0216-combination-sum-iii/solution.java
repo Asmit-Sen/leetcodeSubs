031f5f9700535a2a455368d726d86411ed4b6f05
Asmit Sen <susmita.sen1130@gmail.com>
2025-03-05 12:40:40 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41 MB (83.14%)

--- 0216-combination-sum-iii/solution.java
+++ 0216-combination-sum-iii/solution.java
@@ -1,23 +1,21 @@
 class Solution {
-    void f(int k,int n,int cnt, List<Integer> list, List<List<Integer>> ans )
+    void f(int digit, int k, int n, List<Integer> list, List<List<Integer>> ans)
     {
-        if(n==0 && k==0)
+        if(digit==10)
         {
+            if(k==0 && n==0)
             ans.add(new ArrayList<>(list));
             return;
         }
-        if(k<0) return;
+        list.add(digit);
+        f(digit+1,k-1,n-digit,list,ans);//take the digit
+        list.remove(list.size()-1);
+        f(digit+1,k,n,list,ans);//dont take the digit
 
-        for(int i=cnt;i<10;i++)
-        {
-            list.add(i);
-            f(k-1,n-i,i+1,list,ans);
-            list.remove(list.size()-1);
-        }
     }
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> ans=new ArrayList<>();
-        f(k,n,1,new ArrayList<>(), ans);
+        f(1,k,n,new ArrayList<>(),ans);
         return ans;
     }
 }
