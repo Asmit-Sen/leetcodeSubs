16625fa6cbb87b7837095ebc7c44eac6dd8fcf24
Asmit Sen <susmita.sen1130@gmail.com>
2025-03-11 06:20:36 +0000

    [LeetCode Sync] Runtime - 21 ms (82.52%), Memory - 26.6 MB (79.96%)

---
+++ 1250-longest-common-subsequence/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n=text1.length();
+        int m=text2.length();
+
+        // vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        vector<int> prev(m+1,0);
+
+        for(int i=1;i<=n;i++)
+        {
+            vector<int>cur(m+1,0);
+            for(int j=1;j<=m;j++)
+            {
+                cur[j]=(text1[i-1]==text2[j-1])?(1+prev[j-1]):(max(prev[j],cur[j-1]));
+            }
+            prev=cur;
+        }
+        return prev[m];
+    }
+};
