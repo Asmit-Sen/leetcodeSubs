7498b3b2e102d05e9fd2ec7f424a8de38dadcb1e
Asmit Sen <susmita.sen1130@gmail.com>
2025-06-24 17:41:45 +0000

    [LeetCode Sync] Runtime - 20 ms (75.36%), Memory - 51.4 MB (59.25%)

--- 1250-longest-common-subsequence/solution.java
+++ 1250-longest-common-subsequence/solution.java
@@ -1,21 +1,26 @@
 class Solution {
-    public int longestCommonSubsequence(String s1, String s2) {
-        int m=s1.length(),n= s2.length();
-        int[] prev=new int[n+1], cur=new int[n+1];
-        
-        //shift indices from (0...m-1) by +1 to account for index=-1 as (0....m)
-        
-        //base cases for -ve indices
 
-        for(int i=1;i<=m;i++)
-        {
-            for(int j=1;j<=n;j++)
-            {
-                if(s1.charAt(i-1)==s2.charAt(j-1)) cur[j] = 1+prev[j-1];
-                else cur[j]=(int)Math.max(cur[j-1],prev[j]);
+    int lcs(int i, int j, char[] s1, char[] s2, int dp[][]){
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+
+        if(s1[i]==s2[j]) return dp[i][j]=1+lcs(i-1,j-1,s1,s2,dp);
+        else return dp[i][j]=(int)Math.max(lcs(i-1,j,s1,s2,dp), lcs(i,j-1,s1,s2,dp));
+    }
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n=text1.length();
+        int m=text2.length();
+
+        int dp[][]=new int[n+1][m+1];
+        //0 maps to -1
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1.charAt(i-1)==text2.charAt(j-1)) dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]=(int)Math.max(dp[i-1][j],dp[i][j-1]);
             }
-            prev=cur.clone();
         }
-        return prev[n];
+
+        return dp[n][m];
     }
 }
