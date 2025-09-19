863e705bc7905fe095b129a8ee5639b4722a7aa1
Asmit Sen <susmita.sen1130@gmail.com>
2025-09-19 19:49:01 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.3 MB (82.94%)

---
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    boolean foo(int n){
+        while(n>0){
+            if(n%10==0) return false;
+            n/=10;
+        }
+        return true;
+    }
+    public int[] getNoZeroIntegers(int n) {
+        for(int i=1;i<=n/2;i++){
+            if(foo(i) && foo(n-i))
+            return new int[]{i,n-i};
+        }
+        return new int[]{-1,-1};
+    }
+}
