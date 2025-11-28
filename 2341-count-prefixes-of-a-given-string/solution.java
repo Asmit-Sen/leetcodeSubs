0714fb2574b123ecdf834e6ae5fbd1d4bc6092b9
Asmit Sen <susmita.sen1130@gmail.com>
2025-11-28 18:37:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 45 MB (7.14%)

---
+++ 2341-count-prefixes-of-a-given-string/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int countPrefixes(String[] words, String s) {
+        int c=0;
+        for(String x:words) {
+            if(s.startsWith(x)) c++;
+        }
+        return c;
+    }
+}
