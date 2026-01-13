33fc0bd9e64c362ae539d706a24949f2063c216d
Asmit Sen <susmita.sen1130@gmail.com>
2026-01-13 15:44:47 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 45.6 MB (47.49%)

--- 0110-balanced-binary-tree/solution.java
+++ 0110-balanced-binary-tree/solution.java
@@ -14,39 +14,19 @@
  * }
  */
 class Solution {
-    // int f(TreeNode root, int flag[])
-    // {
-    //     if(root==null) return 0;
-    //     int l=0,r=0;
+    int maxi=0;
+    int foo(TreeNode root){
+        if (root==null) return 0;
 
-    //     l=f(root.left,flag);
-    //     r=f(root.right,flag);
+        int left = foo(root.left);
+        int right = foo(root.right);
+        int d = left - right;
+        maxi=(int)Math.max(maxi, (int)Math.abs(d));
 
-    //     if(Math.abs(l-r)>1)
-    //     flag[0]=0; // false
-
-    //     return 1+(int)Math.max(l,r);
-    // }
-
-    int f(TreeNode root)
-    {
-        if(root==null) return 0;
-        int l=0,r=0;
-
-        l=f(root.left);
-        r=f(root.right);
-
-        if(l==-1 || r==-1) return -1;
-        if(Math.abs(l-r)>1)
-        return -1; // if false gives -1 
-
-        return 1+(int)Math.max(l,r);//if true, returns height and we continue 
+        return 1+(int)Math.max(left,right);
     }
     public boolean isBalanced(TreeNode root) {
-        // int flag[]={1};
-        // f(root,flag);
-        // return (flag[0]==1);
-
-        return f(root)!=-1;
+        foo(root);
+        return maxi<=1;
     }
 }
