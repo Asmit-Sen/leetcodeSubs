2f8462a8aef4d24d210a33e7c55d211c6c6c115e
Asmit Sen <susmita.sen1130@gmail.com>
2026-01-26 19:37:02 +0000

    [LeetCode Sync] Runtime - 21 ms (68.68%), Memory - 64.1 MB (34.79%)

---
+++ 1306-minimum-absolute-difference/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        Arrays.sort(arr);
+        int mini = Integer.MAX_VALUE;
+        for(int i=0;i<arr.length-1;i++)
+            mini=(int)Math.min(mini,arr[i+1]-arr[i]);
+
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i=0;i<arr.length-1;i++){
+            if (arr[i+1]-arr[i]==mini){
+                List<Integer> list = new ArrayList<>();
+                list.add(arr[i]); 
+                list.add(arr[i+1]); 
+                ans.add(list);
+            }
+        }
+
+        return ans;
+    }
+}
