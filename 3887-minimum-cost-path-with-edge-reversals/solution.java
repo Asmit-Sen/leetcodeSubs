05e7339eb81d51fdcebcb25d001f1bd95c948e1a
Asmit Sen <susmita.sen1130@gmail.com>
2026-01-27 14:51:41 +0000

    [LeetCode Sync] Runtime - 130 ms (46.78%), Memory - 193.2 MB (91.69%)

--- 3887-minimum-cost-path-with-edge-reversals/solution.java
+++ 3887-minimum-cost-path-with-edge-reversals/solution.java
@@ -1,56 +1,46 @@
 class Solution {
     public int minCost(int n, int[][] edges) {
-        //construct the adjacency list
-        List<List<List<Integer>>> adj = new ArrayList<>();
-
+        List<List<int[]>> adj = new ArrayList<>();
         for(int i=0;i<n;i++)
             adj.add(new ArrayList<>());
 
-        for(int edge[]:edges){
-            int u=edge[0];
-            int v=edge[1];
-            int w=edge[2];
-            //{v,w}
-            List<Integer> dest = new ArrayList<>();
-            dest.add(v);
-            dest.add(w);
-            // {u->{{v,w},{},{}}}
-            adj.get(u).add(dest);
-            
-            List<Integer> rev = new ArrayList<>();
-            rev.add(u);
-            rev.add(w*2);
-            adj.get(v).add(rev);
+        for(int e[] : edges){
+            int u = e[0];
+            int v = e[1];
+            int w = e[2];
+
+            adj.get(u).add(new int[]{v,w});
+
+            adj.get(v).add(new int[]{u, 2*w});
         }
 
+        //dijsktras on the graph with modified paths
 
-        //dijkstras
-        //{distance, node}
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[0]-b[0]);
-        int dist[]=new int[n];
+        PriorityQueue<int[]> pq = new PriorityQueue<>
+        ((a,b)-> a[1]-b[1]);
+        // (node, dist)
+
+        int dist[] = new int[n];
+        Arrays.fill(dist, (int)1e9);
 
         pq.offer(new int[]{0,0});
-        Arrays.fill(dist,(int)1e9);
         dist[0]=0;
 
         while(!pq.isEmpty()){
-            int top[]=pq.peek();
-            pq.poll();
+            int top[] = pq.poll();
+            int node = top[0];
+            int curr = top[1];
 
-            int d=top[0];
-            int node=top[1];
+            for(int nbrNode[] : adj.get(node)){
+                int nbr = nbrNode[0];
+                int wt = nbrNode[1];
 
-            for(List<Integer> nbr : adj.get(node)){
-                int destination = nbr.get(0);
-                int edgew = nbr.get(1);
-
-                if(d+edgew<dist[destination]){
-                    dist[destination]=d+edgew;
-                    pq.offer(new int[]{dist[destination], destination});
+                if (curr + wt < dist[nbr]){
+                    dist[nbr] = curr+wt;
+                    pq.offer(new int[]{nbr, dist[nbr]});
                 }
             }
         }
-
-        return dist[n-1]==(int)1e9?-1:dist[n-1];
+        return dist[n-1]==(int)1e9 ? -1 : dist[n-1];
     }
 }
