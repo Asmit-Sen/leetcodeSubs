8a75d6afad904c7472722690a42e7704a63f4c5d
Asmit Sen <susmita.sen1130@gmail.com>
2026-03-08 18:37:28 +0000

    [LeetCode Sync] Runtime - 9 ms (36.45%), Memory - 47.6 MB (40.34%)

--- 0417-pacific-atlantic-water-flow/solution.java
+++ 0417-pacific-atlantic-water-flow/solution.java
@@ -3,75 +3,87 @@ class Solution {
         return (i>=0 && j>=0 && i<n && j<m);
     }
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        int n=heights.length;
-        int m=heights[0].length;
+        // cells that can be climbed onto from both sides of the ocean
 
-        int vis1[][]=new int[n][m];
-        int vis2[][]=new int[n][m];
+        int n=heights.length, m=heights[0].length;
 
-        //floodfill from both the oceans
-        //whcichever cell is both in vis1 and vis2 is part of ans
+        int vis1[][]= new int[n][m];
+        int vis2[][]= new int[n][m];
 
-        List<List<Integer>> ans = new ArrayList<>();
-        int dx[]={0,0,-1,1};
+        int dx[]={0,0,1,-1};
         int dy[]={1,-1,0,0};
 
         Queue<int[]> q1 = new LinkedList<>();
-        for(int i=0;i<n;i++) q1.offer(new int[]{i,0});
-        for(int i=0;i<m;i++) q1.offer(new int[]{0,i});
-
-        while(!q1.isEmpty()){
-            int top[], r, c;
-
-            top = q1.poll();
-            r=top[0];
-            c=top[1];
-            vis1[r][c]=1;
-
-            for(int d=0;d<4;d++){
-                int newr= r + dx[d];
-                int newc= c + dy[d];
-                if(isValid(newr, newc, n, m) && vis1[newr][newc]!=1  
-                && heights[newr][newc]>=heights[r][c]){
-                    q1.offer(new int[]{newr, newc});
-                    
-                }
-            }
-        }
-
-        Queue<int[]> q2 = new LinkedList<>();
-        for(int i=0;i<n;i++) q2.offer(new int[]{i,m-1});
-        for(int i=0;i<m;i++) q2.offer(new int[]{n-1,i});
-
-        while(!q2.isEmpty()){
-            int top1[], r1, c1;
-
-            top1= q2.poll();
-            r1=top1[0];
-            c1=top1[1];
-            vis2[r1][c1]=1;
-
-            for(int d=0;d<4;d++){
-                int newr1= r1 + dx[d];
-                int newc1= c1 + dy[d];
-                if(isValid(newr1, newc1, n, m) && vis2[newr1][newc1]!=1  
-                && heights[newr1][newc1]>=heights[r1][c1]){
-                    q2.offer(new int[]{newr1, newc1});
-                    
-                }
-            }
-        }
+        int r=0,c=0,nr=0,nc=0;
 
         for(int i=0;i<n;i++){
+            q1.offer(new int[]{i,0});
+            vis1[i][0]=1;
+        } 
+        for(int i=0;i<m;i++){
+            q1.offer(new int[]{0,i});
+            vis1[0][i]=1;
+        } 
+
+        while(!q1.isEmpty()){
+            int top[]=q1.poll();
+            r=top[0];
+            c=top[1];
+
+            for(int dir=0;dir<4;dir++){
+                nr = r + dx[dir];
+                nc = c + dy[dir];
+
+                if (isValid(nr, nc, n, m) && vis1[nr][nc]==0 && heights[nr][nc]>=heights[r][c]){
+                    vis1[nr][nc]=1;
+                    q1.offer(new int[]{nr,nc});
+                }
+            }
+        }
+
+        r=0;
+        c=0;
+        nr=0;
+        nc=0;
+        Queue<int[]> q2 = new LinkedList<>();
+
+        for(int i=0;i<n;i++){
+            q2.offer(new int[]{i,m-1});
+            vis2[i][m-1]=1;
+        } 
+        for(int i=0;i<m;i++){
+            q2.offer(new int[]{n-1,i});
+            vis2[n-1][i]=1;
+        } 
+
+        while(!q2.isEmpty()){
+            int top1[]=q2.poll();
+            r=top1[0];
+            c=top1[1];
+
+            for(int dir=0;dir<4;dir++){
+                nr = r + dx[dir];
+                nc = c + dy[dir];
+
+                if (isValid(nr, nc, n, m) && vis2[nr][nc]==0 && heights[nr][nc]>=heights[r][c]){
+                    vis2[nr][nc]=1;
+                    q2.offer(new int[]{nr,nc});
+                }
+            }
+        }
+
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(vis1[i][j]==1 && vis2[i][j]==1){   
-                    List<Integer> pair= new ArrayList<>();
+                if (vis1[i][j]==1 && vis2[i][j]==1) {
+                    List<Integer> pair = new ArrayList<>();
                     pair.add(i);
                     pair.add(j);
                     ans.add(pair);
                 }
             }
         }
+
         return ans;
     }
 }
