a56095961392789a6e4ac74825f0b8e290261328
Asmit Sen <susmita.sen1130@gmail.com>
2026-04-13 08:32:02 +0000

    [LeetCode Sync] Runtime - 1 ms (42.30%), Memory - 45 MB (42.61%)

---
+++ 1975-minimum-distance-to-the-target-element/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int getMinDistance(int[] nums, int target, int start) {
+        int ans = nums.length;
+        for(int i=0;i<nums.length;i++){
+            if (nums[i]==target) 
+            ans = (int)Math.min(ans, (int)Math.abs(i-start));
+        }
+        return ans;
+    }
+}
