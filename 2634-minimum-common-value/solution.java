70b6c6a8bdd7a7aceb3ec2019d49f18ec2078f39
Asmit Sen <susmita.sen1130@gmail.com>
2026-05-21 19:14:57 +0000

    [LeetCode Sync] Runtime - 1 ms (97.41%), Memory - 80.1 MB (91.00%)

--- 2634-minimum-common-value/solution.java
+++ 2634-minimum-common-value/solution.java
@@ -1,12 +1,12 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        int l=0,r=0;
-        while(l<nums1.length && r<nums2.length)
-        {
-            if(nums1[l]<nums2[r]) l++;
-            else if(nums1[l]>nums2[r]) r++;
-            else return nums1[l];
-                       
+        int l = 0, r = 0;
+        int n = nums1.length, m = nums2.length;
+        while(l < n && r < m){
+            if (nums1[l] == nums2[r]) return nums1[l];
+
+            if (nums1[l]<nums2[r]) l++;
+            else r++;
         }
         return -1;
     }
