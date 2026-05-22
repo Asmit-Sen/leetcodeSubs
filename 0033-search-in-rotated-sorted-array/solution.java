bc5d8c93c7dddbd9872ea64b23045fb65dc407c7
Asmit Sen <susmita.sen1130@gmail.com>
2026-05-22 18:57:19 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.8 MB (45.65%)

--- 0033-search-in-rotated-sorted-array/solution.java
+++ 0033-search-in-rotated-sorted-array/solution.java
@@ -1,23 +1,20 @@
 class Solution {
-    public int search(int[] arr, int target) {
-        int l=0,r=arr.length-1,mid=0;
-        while(l<=r)
-        {
-            mid=(l+r)/2;
-            if(arr[mid]==target) return mid;
-            if(arr[l]<=arr[mid])//left sorted
-            {
-                if(arr[l]<=target && target<arr[mid])
-                r=mid-1;
-                else
-                l=mid+1;
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int l = 0, h = n-1;
+
+        while(l<=h){
+            int mid = l + ( h - l) / 2;
+            if (target==nums[mid]) return mid;
+            if (nums[l]<=nums[mid]){
+                // left half sorted
+                if (target>=nums[l] && target<=nums[mid]) h=mid-1;
+                else l=mid+1;
             }
-            else//right sorted
-            {
-                if(arr[mid]<target && target<=arr[r])
-                l=mid+1;
-                else
-                r=mid-1;
+            else{
+                // right half sorted
+                if (target>=nums[mid] && target<=nums[h]) l=mid+1;
+                else h=mid-1;
             }
         }
         return -1;
